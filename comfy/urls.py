252b534725fe62612b1407a70fdee459c8fd2899
acskii <andrew.sameh.06@gmail.com>
2025-01-10 23:56:24 +0200

    routing for extract page added

--- comfy/urls.py
+++ comfy/urls.py
@@ -6,6 +6,7 @@ app_name = 'comfy'
 urlpatterns = [
     path('', views.home_view, name='home_index'),
     path('home/', views.home_view, name='home'),
+    path('series/', views.extract_view, name='series'),
     path('signup/', views.register_view, name='signup'),
     path('logout/', views.logout_direct, name='logout'),
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
