2a25ca88171e0bd569c8f3e5e4137ba9a28bcaa1
acskii <andrew.sameh.06@gmail.com>
2025-01-10 23:15:05 +0200

    feat: server-side views
    added view for signing up new users
    added home view
    .. currently can record typed urls
    .> redirects to another view for extraction after.
    .. simple layout
    .. shows currently logged in user
    More features for home page will be added.
    added logout view

--- comfy/views.py
+++ comfy/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.core.cache import cache
+from .extractors.extract import determine_option
+from .utils.cache import cached_scrape
 
-# Create your views here.
+ERR_URL_WRONG = "URL entered is not recognised"
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('comfy:home'))
+    else:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+
+            if form.is_valid():
+                login(request, form.save())
+                return redirect(reverse('comfy:home'))
+        else:
+            form = UserCreationForm()
+
+        return render(request, 'registration/sign_up.html', {"form": form})
+
+@login_required
+def home_view(request):
+    if request.method == 'POST':
+        # User entered a URL in the snatch form
+        if 'home-extract-form' in request.POST:
+            
+            typed_url = request.POST.get('url', None)
+            if typed_url is not None:
+                option = determine_option(typed_url)
+
+                if option is not None:
+                    cache.set('option', option, timeout=3600)
+                    cache.set('series_url', typed_url.strip(), timeout=3600)
+                    return redirect(reverse('comfy:series'))
+            return render(request, 'home/home.html', {"error": ERR_URL_WRONG})
+        
+    return render(request, 'home/home.html')
+
+@login_required
+async def extract_view(request):
+    pass
+
+@login_required
+def logout_direct(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect(reverse('comfy:login'))
