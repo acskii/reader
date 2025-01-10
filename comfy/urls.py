9717c2402686986506e2bd6df7f7ed56af71223a
acskii <andrew.sameh.06@gmail.com>
2025-01-10 20:52:15 +0200

    added login and logout routes in app

--- comfy/urls.py
+++ comfy/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'comfy'
@@ -6,4 +7,6 @@ urlpatterns = [
     path('', views.home_view, name='home_index'),
     path('home/', views.home_view, name='home'),
     path('signup/', views.register_view, name='signup'),
+    path('logout/', views.logout_direct, name='logout'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
 ]
