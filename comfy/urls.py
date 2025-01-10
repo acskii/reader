2a20a6f783498e0ddc06771411585dfd8e168f5c
acskii <andrew.sameh.06@gmail.com>
2025-01-10 19:53:10 +0200

    feat: app namespace and new routes
    added app name for namespace
    added sign up page route
    now on website load, it routes to app

--- comfy/urls.py
+++ comfy/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'comfy'
 urlpatterns = [
+    path('', views.home_view, name='home_index'),
     path('home/', views.home_view, name='home'),
+    path('signup/', views.register_view, name='signup'),
 ]
