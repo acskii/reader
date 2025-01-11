e56937dd6555daa5e11bb2813b4f6bb496c7302a
acskii <andrew.sameh.06@gmail.com>
2025-01-11 21:08:00 +0200

    feat: visit history database model

--- comfy/models.py
+++ comfy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class VisitHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='visited')
+    title = models.CharField(max_length=100)
+    series_url = models.CharField(max_length=400)
+    preview_url = models.CharField(max_length=400)
+    website_option = models.PositiveSmallIntegerField()
+    logo = models.CharField(max_length=400)
+    last_visited = models.DateTimeField(auto_now=True)
+    last_read = models.CharField(max_length=100)
