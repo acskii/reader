d0e088b9f5e6397fbc50270b4e186037eb9d6613
acskii <andrew.sameh.06@gmail.com>
2025-01-14 00:10:24 +0200

    feat: add search scrape function

--- comfy/utils/scrape.py
+++ comfy/utils/scrape.py
@@ -10,12 +10,21 @@ async def scrape_info(website_option: str, url: str):
 
     return None
 
+async def scrape_results(website_option: str, keywords: str, page: int):
+    extractor = get_extractor(website_option)
+    
+    if extractor is not None:
+        results = await extractor.parse_search(keywords, page)
+        
+        if results is not None: return results
+
+    return None
+
 async def scrape_chapter(website_option: str, url: str):
     extractor = get_extractor(website_option)
     
     if extractor is not None:
         results = await extractor.parse_chapter(url)
-        
         if results is not None: return results
 
     return None
